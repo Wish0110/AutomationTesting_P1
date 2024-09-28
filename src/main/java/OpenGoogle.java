732a4057d9a31066edf4daa42821f9254b2c9c14
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-09-29 00:57:24 +0530

    get google

--- src/main/java/OpenGoogle.java
+++ src/main/java/OpenGoogle.java
@@ -1,7 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class OpenGoogle {
     public static void main(String[] args) {
 
         System.setProperty("webdriver.chrome.driver","C:\\webdrivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
 
+        driver.get("https://www.google.com");
+
+        driver.quit();
     }
 }
