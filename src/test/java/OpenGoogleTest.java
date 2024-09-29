f2e3a6e44583eb7ef47045cb005278af3aca6368
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-09-29 14:21:36 +0530

    Selenium Manager

--- src/test/java/OpenGoogleTest.java
+++ src/test/java/OpenGoogleTest.java
@@ -1,11 +1,10 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class OpenGoogle {
-    public static void main(String[] args) {
-
+public class OpenGoogleTest {
+    public void googleTest(){
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com");
-        driver.quit();
+
     }
 }
