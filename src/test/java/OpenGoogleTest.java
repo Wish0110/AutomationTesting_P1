2a9ec9478313bafd3bdfce4ddafb7a38f160aed2
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-09-29 14:29:06 +0530

    setup TestNG

--- src/test/java/OpenGoogleTest.java
+++ src/test/java/OpenGoogleTest.java
@@ -1,10 +1,14 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class OpenGoogleTest {
+
+    @Test
+
     public void googleTest(){
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com");
-
+        driver.quit();
     }
 }
